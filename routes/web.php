bf305f03c39b71b7a0f44265c8fd13f3ce7d1142
Pedrinh0Silva <0001153392@senaimgaluno.com.br>
2026-03-04 20:02:10 -0300

    mod routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,12 @@ use App\Http\Controllers\PedidoController;
 */
 
 // Rota da Página Inicial
-// Por enquanto, vamos redirecionar quem entra no site direto para a lista de flores
+
 Route::get('/', function () {
     return redirect()->route('flores.index');
 });
 
 // Rotas de Flores (Produtos)
-// O comando 'resource' cria AUTOMATICAMENTE as 7 rotas (index, create, store, edit, etc.)
 Route::resource('flores', FlorController::class);
 
 // Rotas de Clientes
@@ -28,9 +27,3 @@ Route::resource('clientes', ClienteController::class);
 // Rotas de Pedidos (Vendas)
 Route::resource('pedidos', PedidoController::class);
 
-// Rota de Teste para vermos se a autenticação funciona (futuramente)
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/auth.php'; // Mantém as rotas de login do Laravel Breeze/Jetstream se tiver instalado
